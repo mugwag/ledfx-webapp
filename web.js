6700e9e5a71cd75cd88c18481a8bcf49bb208409
Leon <leonpaulharris@gmail.com>
2017-04-06 16:23:45 +0100

    logger

--- web.js
+++ web.js
@@ -4,6 +4,5 @@ var gzippo = require('gzippo');
 var express = require('express');
 var nodeApp = express();
 
-nodeApp.use(express.logger('dev'));
 nodeApp.use(gzippo.staticGzip('' + __dirname + '/app'));
 nodeApp.listen(process.env.PORT || 5000);
